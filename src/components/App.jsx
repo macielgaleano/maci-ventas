7fd0bc92a340eaa0e74e2a11f78c345286ded5b9
Maciel Galeano <macielgaleano.jh@gmail.com>
2021-05-03 21:53:39 -0300

    Config pretties, eslint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import "../styles/components/app.css";
+import React from 'react';
+import '../styles/components/app.css';
 
 export default function App() {
   return <div>Hola pedro</div>;
