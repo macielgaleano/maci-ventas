8ec4db390448e5e04e0e02d1dd3d4573569827a0
Maciel Galeano <macielgaleano.jh@gmail.com>
2021-05-03 21:18:24 -0300

    Sass webpack

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,10 @@ module.exports = {
           "css-loader",
         ],
       },
+      {
+        test: /\.scss$/,
+        loader: [MiniCSSExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
