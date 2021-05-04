1c73f8471f9f537af0fc768970de55a3dd993e73
Maciel Galeano <macielgaleano.jh@gmail.com>
2021-05-03 21:12:56 -0300

    css web pack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -27,6 +28,15 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+        ],
+      },
     ],
   },
   plugins: [
@@ -34,12 +44,15 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html",
     }),
+
+    new MiniCssExtractPlugin({
+      filename: "assests[name].css",
+    }),
   ],
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
-    port: 3000,
+    port: 3005,
   },
 };
-
 //Developed by GALLARDO - github: @macielgaleano
