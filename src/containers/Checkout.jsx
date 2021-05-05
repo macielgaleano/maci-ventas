98cb1041d3984cb46390c2cd4e597401b92b408c
Maciel Galeano <macielgaleano.jh@gmail.com>
2021-05-04 22:07:04 -0300

    Information

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import '../styles/components/Checkout.css';
 
 const Checkout = () => {
   return (
-    <h1>Checkout</h1>
+    <div className="Checkout">
+      <div className="Checkout-content">
+        <h3>Lista de pedidos:</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Loli #1</h4>
+            <span>$10</span>
+          </div>
+          <button type="button">Eliminar</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Precio Total: $10</h3>
+        <button type="button">Continuar pedidio</button>
+      </div>
+    </div>
   );
-}
+};
 
 export default Checkout;
--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import '../styles/components/Information.css';
 
 const Information = () => {
   return (
-    <h1>Information</h1>
+    <div className="Information">
+      <div className="Information-Contant">
+        <div className="Information-head">
+          <h2>Informacion de contacto</h2>
+        </div>
+        <div className="Information-form">
+          <form>
+            <input type="text" placeholder="Nombre completo" name="name" />
+            <input type="email" placeholder="Correo electronico" name="email" />
+            <input type="text" placeholder="Direccion" name="address" />
+            <input type="text" placeholder="Apto" name="apto" />
+            <input type="text" placeholder="Ciudad" name="city" />
+            <input type="text" placeholder="Country" name="country" />
+            <input type="text" placeholder="Estado" name="state" />
+            <input type="text" placeholder="Codigo postal" name="cp" />
+            <input type="text" placeholder="Telofono" name="phone" />
+          </form>
+        </div>
+        <div className="Information-buttons">
+          <div className="Information-back">Regresar</div>
+          <div className="Information-next">Pagar</div>
+        </div>
+      </div>
+      <div className="Information-sidebar">
+        <h3>Pedido: </h3>
+        <div className="Information-element">
+          <h4>Item name </h4>
+          <span>$10</span>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Information;
