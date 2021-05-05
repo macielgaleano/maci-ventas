a9503d9f546e507ba950c0587427bfbb5679ddb4
Maciel Galeano <macielgaleano.jh@gmail.com>
2021-05-04 22:45:29 -0300

    Paymet checkout

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../styles/components/Payment.css';
 
 const Payment = () => {
   return (
-    <h1>Payment</h1>
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resumen del pedido:</h3>
+        <div className="Payment-button">Boton de pago con Paypal</div>
+      </div>
+      <div></div>
+    </div>
   );
-}
+};
 
 export default Payment;
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../styles/components/Success.css';
 
 const Success = () => {
   return (
-    <h1>Success</h1>
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Gracias por tu compra!</h2>
+        <span>Tu pedido llegara en 3 dias a tu direccion</span>
+        <div className="Success-map">Google maps</div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Success;
